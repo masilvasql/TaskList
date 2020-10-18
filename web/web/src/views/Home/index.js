de4a015d00e889e30c27f4eabdee2ff2c6925880
Marcelo Abrão da Silva <masilvasql@gmail.com>
2020-10-18 12:26:11 -0300

    Iniciando desenvolvimento Web, preparação do ambiente, criação de HEADER e FOOTER com Styled Components

---
+++ web/web/src/views/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "./style";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function Home() {
+  return (
+    <S.Container>
+      <Header />
+      <Footer />
+    </S.Container>
+  );
+}
+
+export default Home;
