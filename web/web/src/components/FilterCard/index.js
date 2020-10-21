4ab6ec08b3e44d74ad35877c12c59bf6e5fe6ba3
Marcelo Abrão da Silva <masilvasql@gmail.com>
2020-10-21 10:43:09 -0300

    criado componente de filtro

---
+++ web/web/src/components/FilterCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+import filter from "../../assets/filter.png";
+
+function FilterCard({ title, actived }) {
+  return (
+    <S.Container actived={actived}>
+      <img src={filter} alt="filtro" />
+      <span>{title}</span>
+    </S.Container>
+  );
+}
+
+export default FilterCard;
